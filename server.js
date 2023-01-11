bfc2f581f2498034809ac2770dc2af67498c1ba5
Adiba Belayete <adiba.du.bd@gmail.com>
2023-01-11 10:51:26 +0000

    modified listen process

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 9000
 // const fakeBookingsData = require("./data/FakeBookingsData")
 
-app.listen(PORT, function () {
+app.listen(process.env.PORT || PORT , function () {
   console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
 });
 
